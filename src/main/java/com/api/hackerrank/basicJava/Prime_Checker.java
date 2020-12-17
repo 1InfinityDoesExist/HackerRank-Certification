99cb10350ae7686f0c8c1da487ecbb37cc712919
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-12-17 08:38:09 +0530

    prime_number_checker

---
+++ src/main/java/com/api/hackerrank/basicJava/Prime_Checker.java
@@ -0,0 +1,55 @@
+package com.api.hackerrank.basicJava;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+import static java.lang.System.in;
+
+class Prime {
+
+	public void checkPrime(int... n1) {
+		for (int x : n1) {
+			if (BigInteger.valueOf(x).isProbablePrime(10)) {
+				System.out.print(x + " ");
+			}
+		}
+		System.out.println();
+	}
+}
+
+public class Prime_Checker {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			int n1 = Integer.parseInt(br.readLine());
+			int n2 = Integer.parseInt(br.readLine());
+			int n3 = Integer.parseInt(br.readLine());
+			int n4 = Integer.parseInt(br.readLine());
+			int n5 = Integer.parseInt(br.readLine());
+			Prime ob = new Prime();
+			ob.checkPrime(n1);
+			ob.checkPrime(n1, n2);
+			ob.checkPrime(n1, n2, n3);
+			ob.checkPrime(n1, n2, n3, n4, n5);
+			Method[] methods = Prime.class.getDeclaredMethods();
+			Set<String> set = new HashSet<>();
+			boolean overload = false;
+			for (int i = 0; i < methods.length; i++) {
+				if (set.contains(methods[i].getName())) {
+					overload = true;
+					break;
+				}
+				set.add(methods[i].getName());
+			}
+			if (overload) {
+				throw new Exception("Overloading not allowed");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
